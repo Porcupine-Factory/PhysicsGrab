63fe4c00727cd3b9d7da97df112c3ab5a709b47f
Nana Yellen <NanaYellen@Tutanota.com>
2023-09-24 00:49:09 -0400

    Added raycast query to check entity's grounded state

--- Code/Source/Clients/RaycastTest.cpp
+++ Code/Source/Clients/RaycastTest.cpp
@@ -1,28 +1,69 @@
 #include "RaycastTest.h"
+
+#include <AzCore/Component/Entity.h>
+#include <AzCore/Component/TransformBus.h>
 #include <AzCore/Serialization/EditContext.h>
+#include <AzFramework/Physics/SystemBus.h>
+#include <System/PhysXSystem.h>
 
-using namespace TestGem;
-
-void RaycastTest::Reflect(AZ::ReflectContext* reflection)
+namespace TestGem
 {
-	//AZ_UNUSED(reflection);
-	auto sc = azrtti_cast<AZ::SerializeContext*>(reflection);
-	if (!sc) return;
+    void RaycastTest::Reflect(AZ::ReflectContext* rc)
+    {
+        if (auto sc = azrtti_cast<AZ::SerializeContext*>(rc))
+        {
+            sc->Class<RaycastTest, AZ::Component>()
+                ->Version(1);
 
-	sc->Class<RaycastTest, Component>()
-		->Version(1);
+            if (AZ::EditContext* ec = sc->GetEditContext())
+            {
+                using namespace AZ::Edit;
+                ec->Class<RaycastTest>(
+                    "Raycast Test",
+                    "[Component to test raycast funcionality]")
+                    ->ClassElement(ClassElements::EditorData, "")
+                    ->Attribute(
+                        Attributes::AppearsInAddComponentMenu,
+                        AZ_CRC_CE("Game"));
+            }
+        }
+    }
+	void RaycastTest::Activate()
+	{
+		AZ::TickBus::Handler::BusConnect();
+	}
 
-	AZ::EditContext* ec = sc->GetEditContext();
-	if (!ec) return;
+	void RaycastTest::Deactivate()
+	{
+		AZ::TickBus::Handler::BusDisconnect();
+	}
 
-	using namespace AZ::Edit::Attributes;
+	void RaycastTest::OnTick(float, AZ::ScriptTimePoint)
+	{
+		RaycastCheck();
+	}
 
-		// reflection of this component for O3DE Editor
+	void RaycastTest::RaycastCheck()
+	{   
+        // Get our entity's local translation
+        AZ::Vector3 currentTranslation = GetEntity()->GetTransform()->GetLocalTranslation();
 
-		ec->Class<RaycastTest>("Raycast Test", "[Test new component]")
-		->ClassElement(AZ::Edit::ClassElements::EditorData, "")
-		->Attribute(AppearsInAddComponentMenu, AZ_CRC("Game"))
-		->Attribute(Category, "TestGem");
+        // Perform a raycast query to check if entity is grounded
+        auto* sceneInterface = AZ::Interface<AzPhysics::SceneInterface>::Get();
+
+        AzPhysics::RayCastRequest request;
+        request.m_start = currentTranslation;
+        request.m_direction = AZ::Vector3(0.0f, 0.0f, -1.0f);
+        request.m_distance = 0.3f;
+
+        AzPhysics::SceneHandle sceneHandle = sceneInterface->GetSceneHandle(AzPhysics::DefaultPhysicsSceneName);
+        AzPhysics::SceneQueryHits hits = sceneInterface->QueryScene(sceneHandle, &request);
+
+        m_grounded = hits ? true : false;
+
+        // Print entity's grounded state
+        AZ_Printf("", "%s", m_grounded ? "Grounded" : "NOT Grounded");
+	}
 }
 
 
--- Code/Source/Clients/RaycastTest.h
+++ Code/Source/Clients/RaycastTest.h
@@ -1,20 +1,32 @@
 #pragma once
 #include <AzCore/Component/Component.h>
-#include <StartingPointInput/InputEventNotificationBus.h>
+#include <AzCore/Component/TickBus.h>
+#include <AzCore/Math/Vector3.h>
+#include <AzFramework/Physics/Common/PhysicsSceneQueries.h>
+//#include <PhysXCharacters/API/CharacterController.h>
 
 namespace TestGem
 {
 	// An example of a simple O3DE component
-	class RaycastTest : public AZ::Component
+	class RaycastTest 
+		: public AZ::Component
+		, public AZ::TickBus::Handler
 	{
 	public:
 		AZ_COMPONENT(RaycastTest, "{81F030E1-6EBC-46E6-97D0-DA7B822239E6}");
 
-		// AZ::Component overrides
-		void Activate() override {}
-		void Deactivate() override {}
-
 		// Provide runtime reflection, if any
-		static void Reflect(AZ::ReflectContext* reflection);
+		static void Reflect(AZ::ReflectContext* rc);
+
+		// AZ::Component overrides
+		void Activate() override;
+		void Deactivate() override;
+
+		void OnTick(float deltaTime, AZ::ScriptTimePoint) override;
+
+	private:
+		void RaycastCheck();
+
+		bool m_grounded = true;
 	};
 }
