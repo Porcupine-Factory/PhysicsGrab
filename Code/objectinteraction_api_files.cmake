4d05debe11366a59d56233e05ab6fe46128907de
NanaYellen <nanayellen@tutanota.com>
2025-04-01 21:54:22 -0400

    Remove FastNoise and FirstPersonController Gems from CMakeLists dependencies. Add optional file path to FirstPersonControllerComponentBus.h depending on whether FirstPersonController is cloned in the Project Manager or in a CLI.

--- Code/objectinteraction_api_files.cmake
+++ Code/objectinteraction_api_files.cmake
@@ -1,6 +1,12 @@
+find_path(FPC_INCLUDE_DIR
+    NAMES FirstPersonController/FirstPersonControllerComponentBus.h
+    PATHS
+        "${LY_PROJECTS}/../../Gems/FirstPersonController/Code/Include"
+        "${LY_PROJECTS}/../../Gems/FirstPersonController/1.0.0/Code/Include"
+)
 
 set(FILES
     Include/ObjectInteraction/ObjectInteractionBus.h
     Include/ObjectInteraction/ObjectInteractionComponentBus.h
-	${LY_PROJECTS}/../../Gems/FirstPersonController/Code/Include/FirstPersonController/FirstPersonControllerComponentBus.h
+    ${FPC_INCLUDE_DIR}/FirstPersonController/FirstPersonControllerComponentBus.h
 )
